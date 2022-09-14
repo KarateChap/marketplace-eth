31838c8970a7b624c954365c2397a068b2a96979
gab <gabrielmarianoofficial@gmail.com>
2022-09-14 15:11:17 +0800

    added marketplace page

--- components/providers/hooks/useAccount.js
+++ components/providers/hooks/useAccount.js
@@ -1,18 +1,30 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
+import useSWR from "swr";
 
-export const handler = (web3) => () => {
-  const [account, setAccount] = useState(null);
+const adminAddresses = {
+  "0xefa315d014d0cc9b34370d0ba4f51a11d27a1e3c5d58159156ef28c604cfe06a": true,
+};
+
+export const handler = (web3, provider) => () => {
+  const { data, mutate, ...rest } = useSWR(
+    () => (web3 ? "web3/accounts" : null),
+    async () => {
+      const accounts = await web3.eth.getAccounts();
+      return accounts[0];
+    }
+  );
 
   useEffect(() => {
-    const getAccount = async () => {
-      const accounts = await web3.eth.getAccounts();
-      setAccount(accounts[0]);
-    };
-
-    web3 && getAccount();
-  }, [web3]);
+    provider &&
+      provider.on("accountsChanged", (accounts) => mutate(accounts[0] ?? null));
+  }, [provider]);
 
   return {
-    account,
+    account: {
+      data,
+      isAdmin: (data && adminAddresses[web3.utils.keccak256(data)]) ?? false,
+      mutate,
+      ...rest,
+    },
   };
 };
--- components/ui/common/navbar/index.js
+++ components/ui/common/navbar/index.js
@@ -1,14 +1,13 @@
 import { useWeb3 } from "@components/providers";
 import Link from "next/link";
 import { Button } from "@components/ui/common";
-import { useAccount } from "@components/web3/hooks/useAccount";
+import { useAccount } from "@components/hooks/web3/useAccount";
 
 export default function Navbar() {
   const { connect, isLoading, isWeb3Loaded } = useWeb3();
   const { account } = useAccount();
   return (
     <section>
-      {account}
       <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
         <nav className="relative" aria-label="Global">
           <div className="flex justify-between items-center">
@@ -40,7 +39,13 @@ export default function Navbar() {
                   Loading...
                 </Button>
               ) : isWeb3Loaded ? (
-                <Button onClick={connect}>Connect</Button>
+                account.data ? (
+                  <Button className="cursor-default" hoverable={false}>
+                    Hi There {account.isAdmin && "Admin"}
+                  </Button>
+                ) : (
+                  <Button onClick={connect}>Connect</Button>
+                )
               ) : (
                 <Button
                   onClick={() => window.open("https://metamask.io/", "_blank")}
@@ -52,6 +57,13 @@ export default function Navbar() {
           </div>
         </nav>
       </div>
+      {account.data && (
+        <div className="flex justify-end sm:px-6 lg:px-8 pt-1">
+          <div className="text-white bg-indigo-600 rounded-md p-2">
+            {account.data}
+          </div>
+        </div>
+      )}
     </section>
   );
 }
